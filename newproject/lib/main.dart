bfc7b38b2559cd44aeea798ecfe0392d298a0d7b
Remotely-X <remotelyxsoftware@gmail.com>
2026-04-20 18:01:07 -0400

    first commit

---
+++ newproject/lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const MyHomePage2(),
+    );
+  }
+}
+
+
+
+
+
+class MyHomePage2 extends StatelessWidget {
+  const MyHomePage2({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("My App"),
+        backgroundColor: Colors.red,
+      ),
+      body: Container(
+      color: Colors.blue,
+      child:  Text("This is the App"), ),
+
+    );
+  }
+}
